f2d45fdfdbf93ab655e3466a2395220c9eb60d4b
apsrajput <apsrajput008@gmail.com>
2020-07-19 17:17:09 +0530

    added student index route

---
+++ resources/views/students/students.blade.php
@@ -0,0 +1,31 @@
+@extends('layout')
+
+@section('content')
+
+<style>
+    .container {
+      max-width: 450px;
+    }
+    .push-top {
+      margin-top: 50px;
+    }
+</style>
+
+<div class="card push-top">
+  <div class="card-header">
+    Students
+  </div>
+  <div class="card-body">
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+      </div><br />
+    @endif
+      
+  </div>
+</div>
+@endsection
